f7d029e3722ac9706c90784c80bf6fd6e0b98102
Neil <neil.mccaffrey83@gmail.com>
2024-07-30 19:29:07 -0400

    about page update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,62 @@ const About: React.FC = () => {
   return (
     <>
       <h1 className="text-6xl dark:text-white text-center">Skills</h1>
-      <div className="flex flex-row space-x-16">
-        <div className="flex-grow max-w-lg mx-auto p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+      <div className="flex flex-wrap justify-center space-x-2">
+        <div className="flex-grow w-80 p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+          <p className="dark:text-white text-4xl">Languages</p>
+          <p className="dark:text-white mt-4 text-2xl">
+            • JavaScript
+            <br />• HTML/CSS
+            <br />• TypeScript
+            <br />• Java
+            <br />• C++
+            <br />• Python
+          </p>
+        </div>
+        <div className="flex-grow w-80 p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+          <p className="dark:text-white text-4xl">Front-End</p>
+          <p className="dark:text-white mt-4 text-2xl">
+            • React.js
+            <br />• React Native
+            <br />• Next.js
+            <br />• Bootstrap
+            <br />• Tailwind CSS
+          </p>
+        </div>
+        <div className="flex-grow w-80 p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+          <p className="dark:text-white text-4xl">Back-End</p>
+          <p className="dark:text-white mt-4 text-2xl">
+            • Firebase
+            <br />• AWS
+            <br />• REST
+            <br />• Node/Express.js
+          </p>
+        </div>
+        <div className="flex-grow w-80 p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+          <p className="dark:text-white text-4xl">Database Technologies</p>
+          <p className="dark:text-white mt-4 text-2xl">
+            • Firebase Firestore
+            <br />• SQL
+            <br />• NoSQL
+          </p>
+        </div>
+        <div className="flex-grow w-80 p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+          <p className="dark:text-white text-4xl">DevOps and Tools</p>
+          <p className="dark:text-white mt-4 text-2xl">
+            • Git
+            <br />• Fastlane
+            <br />• CI/CD
+          </p>
+        </div>
+        <div className="flex-grow w-80 p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+          <p className="dark:text-white text-4xl">Cloud Services</p>
+          <p className="dark:text-white mt-4 text-2xl">
+            • Firebase
+            <br />• AWS
+            <br />• Google Cloud
+          </p>
+        </div>
+        {/* <div className="flex-grow max-w-lg mx-auto p-4 border border-gray-300 rounded-lg shadow-md mt-8">
           <p className="dark:text-white text-4xl">Languages</p>
           <p className="dark:text-white mt-4 text-2xl">
             • JavaScript
@@ -34,7 +88,7 @@ const About: React.FC = () => {
             <br />• REST
             <br />• Node/Express.js
           </p>
-        </div>
+        </div> */}
       </div>
     </>
   );
