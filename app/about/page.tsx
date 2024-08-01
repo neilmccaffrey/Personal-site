aeff0fe272b7cd19a4360c3f00caa3c3da9457a3
Neil <neil.mccaffrey83@gmail.com>
2024-07-31 22:07:51 -0400

    made cards grid for uniformity

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const About: React.FC = () => {
   return (
     <>
-      <h1 className="text-6xl dark:text-white text-center">Skills</h1>
-      <div className="flex flex-wrap justify-center space-x-2">
-        <div className="flex-grow w-80 p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+      <h1 className="text-6xl dark:text-white text-center mb-4">Skills</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
+        <div className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md ">
           <p className="dark:text-white text-4xl">Languages</p>
           <p className="dark:text-white mt-4 text-2xl">
             • JavaScript
@@ -16,7 +16,7 @@ const About: React.FC = () => {
             <br />• Python
           </p>
         </div>
-        <div className="flex-grow w-80 p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+        <div className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md ">
           <p className="dark:text-white text-4xl">Front-End</p>
           <p className="dark:text-white mt-4 text-2xl">
             • React.js
@@ -26,7 +26,7 @@ const About: React.FC = () => {
             <br />• Tailwind CSS
           </p>
         </div>
-        <div className="flex-grow w-80 p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+        <div className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md ">
           <p className="dark:text-white text-4xl">Back-End</p>
           <p className="dark:text-white mt-4 text-2xl">
             • Firebase
@@ -35,7 +35,7 @@ const About: React.FC = () => {
             <br />• Node/Express.js
           </p>
         </div>
-        <div className="flex-grow w-80 p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+        <div className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md ">
           <p className="dark:text-white text-4xl">Database Technologies</p>
           <p className="dark:text-white mt-4 text-2xl">
             • Firebase Firestore
@@ -43,15 +43,16 @@ const About: React.FC = () => {
             <br />• NoSQL
           </p>
         </div>
-        <div className="flex-grow w-80 p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+        <div className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md ">
           <p className="dark:text-white text-4xl">DevOps and Tools</p>
           <p className="dark:text-white mt-4 text-2xl">
             • Git
+            <br />• GitHub Actions
             <br />• Fastlane
             <br />• CI/CD
           </p>
         </div>
-        <div className="flex-grow w-80 p-4 border border-gray-300 rounded-lg shadow-md mt-8">
+        <div className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md ">
           <p className="dark:text-white text-4xl">Cloud Services</p>
           <p className="dark:text-white mt-4 text-2xl">
             • Firebase
@@ -59,36 +60,6 @@ const About: React.FC = () => {
             <br />• Google Cloud
           </p>
         </div>
-        {/* <div className="flex-grow max-w-lg mx-auto p-4 border border-gray-300 rounded-lg shadow-md mt-8">
-          <p className="dark:text-white text-4xl">Languages</p>
-          <p className="dark:text-white mt-4 text-2xl">
-            • JavaScript
-            <br />• HTML/CSS
-            <br />• TypeScript
-            <br />• Java
-            <br />• C++
-            <br />• Python
-          </p>
-        </div>
-        <div className="flex-grow max-w-lg mx-auto p-4 border border-gray-300 rounded-lg shadow-md mt-8">
-          <p className="dark:text-white text-4xl">Front-End</p>
-          <p className="dark:text-white mt-4 text-2xl">
-            • React.js
-            <br />• React Native
-            <br />• Next.js
-            <br />• Bootstrap
-            <br />• Tailwind
-          </p>
-        </div>
-        <div className="flex-grow max-w-lg mx-auto p-4 border border-gray-300 rounded-lg shadow-md mt-8">
-          <p className="dark:text-white text-4xl">Back-End</p>
-          <p className="dark:text-white mt-4 text-2xl">
-            • Firebase
-            <br />• AWS
-            <br />• REST
-            <br />• Node/Express.js
-          </p>
-        </div> */}
       </div>
     </>
   );
