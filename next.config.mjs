e6be99c4cac6d59fcd527961a691ff88436a5f3e
Neil <neil.mccaffrey83@gmail.com>
2024-08-03 22:05:23 -0400

    dotenv

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_SECURE: process.env.SMTP_SECURE,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASS: process.env.SMTP_PASS,
+    RECEIVER_EMAIL: process.env.RECEIVER_EMAIL,
+  },
+};
 
 export default nextConfig;
