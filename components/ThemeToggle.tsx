20fbb8b2a4c3d5cd750bed07d326bda17a657ac9
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-01-10 21:26:15 -0500

    changed theme to default to dark

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,36 +1,36 @@
-"use client";
-import React, { useEffect, useState } from "react";
+'use client';
+import React, { useEffect, useState } from 'react';
 
-import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ThemeToggle: React.FC = () => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState('dark');
 
   //set dark/light mode remember user preference
   useEffect(() => {
     if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
     ) {
-      document.documentElement.classList.add("dark");
-      setTheme("dark");
+      document.documentElement.classList.add('dark');
+      setTheme('dark');
     } else {
-      document.documentElement.classList.remove("dark");
-      setTheme("light");
+      document.documentElement.classList.remove('dark');
+      setTheme('light');
     }
   }, []);
 
   const toggleTheme = () => {
-    if (theme === "light") {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-      setTheme("dark");
+    if (theme === 'light') {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+      setTheme('dark');
     } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-      setTheme("light");
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+      setTheme('light');
     }
   };
 
@@ -46,13 +46,13 @@ const ThemeToggle: React.FC = () => {
           id="theme-toggle"
           type="checkbox"
           className="sr-only"
-          checked={theme === "dark"}
+          checked={theme === 'dark'}
           onChange={toggleTheme}
         />
         <div className="block w-11 h-6 bg-gray-200 rounded-full dark:bg-gray-700"></div>
         <div
           className={`absolute left-1 top-1 w-4 h-4 bg-white border-2 border-gray-300 rounded-full transition-transform transform ${
-            theme === "dark" ? "translate-x-5" : ""
+            theme === 'dark' ? 'translate-x-5' : ''
           }`}
         ></div>
         <span className="sr-only">Toggle theme</span>
