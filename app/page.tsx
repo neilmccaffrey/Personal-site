7a730be827ff2707c3599c44430ce7c3868eb1d4
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-01-10 21:31:14 -0500

    scale on learn more button

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,11 @@ export default function Page() {
           available for download{' '}
         </p>
         <Link href="/apps">
-          <button className="bg-customDarkText text-white font-bold py-2 px-4 rounded shadow-lg hover:bg-purple-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-customDarkText mt-4">
+          <button
+            className="bg-customDarkText text-white font-bold py-2 px-4 rounded shadow-lg 
+          hover:bg-purple-300 focus:outline-none focus:ring-2 focus:ring-offset-2 
+          focus:ring-customDarkText mt-4 transition-transform duration-200 hover:scale-110"
+          >
             Learn More
           </button>
         </Link>
