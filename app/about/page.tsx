f590baca4fb5fede61433f5f61c27babbdb62f1b
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-01-10 21:04:41 -0500

    added framer-motion and animated cards on about screen

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,32 @@
+'use client'; //framer-motion on works on client side
+
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const About: React.FC = () => {
+  const cardsTop = {
+    hidden: { opacity: 0, y: -150 }, // Start off screen at the top
+    visible: { opacity: 1, y: 0 }, // Move into view
+  };
+
+  const cardsBottom = {
+    hidden: { opacity: 0, y: 150 }, // Start off screen at the bottom
+    visible: { opacity: 1, y: 0 }, // Move into view
+  };
   return (
     <>
       <h1 className="text-6xl dark:text-white text-center mb-4">Skills</h1>
       {/* grid for uniformity */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
-        <div className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md ">
+        {/* Top 3 Cards */}
+        <motion.div
+          className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={cardsTop}
+          transition={{ duration: 0.8 }}
+        >
           <p className="dark:text-white text-4xl">Languages</p>
           <p className="dark:text-white mt-4 text-2xl">
             • JavaScript
@@ -16,8 +36,15 @@ const About: React.FC = () => {
             <br />• C++
             <br />• Python
           </p>
-        </div>
-        <div className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md ">
+        </motion.div>
+        <motion.div
+          className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={cardsTop}
+          transition={{ duration: 0.8 }}
+        >
           <p className="dark:text-white text-4xl">Front-End</p>
           <p className="dark:text-white mt-4 text-2xl">
             • React.js
@@ -26,8 +53,15 @@ const About: React.FC = () => {
             <br />• Bootstrap
             <br />• Tailwind CSS
           </p>
-        </div>
-        <div className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md ">
+        </motion.div>
+        <motion.div
+          className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={cardsTop}
+          transition={{ duration: 0.8 }}
+        >
           <p className="dark:text-white text-4xl">Back-End</p>
           <p className="dark:text-white mt-4 text-2xl">
             • Firebase
@@ -35,16 +69,32 @@ const About: React.FC = () => {
             <br />• REST
             <br />• Node/Express.js
           </p>
-        </div>
-        <div className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md ">
+        </motion.div>
+
+        {/* Bottom 3 Cards */}
+        <motion.div
+          className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={cardsBottom}
+          transition={{ duration: 0.8 }}
+        >
           <p className="dark:text-white text-4xl">Database Technologies</p>
           <p className="dark:text-white mt-4 text-2xl">
             • Firebase Firestore
             <br />• SQL
             <br />• NoSQL
           </p>
-        </div>
-        <div className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md ">
+        </motion.div>
+        <motion.div
+          className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={cardsBottom}
+          transition={{ duration: 0.8 }}
+        >
           <p className="dark:text-white text-4xl">DevOps and Tools</p>
           <p className="dark:text-white mt-4 text-2xl">
             • Git
@@ -52,15 +102,22 @@ const About: React.FC = () => {
             <br />• Fastlane
             <br />• CI/CD
           </p>
-        </div>
-        <div className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md ">
+        </motion.div>
+        <motion.div
+          className="w-full max-w-md p-2 border border-gray-300 rounded-lg shadow-md"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={cardsBottom}
+          transition={{ duration: 0.8 }}
+        >
           <p className="dark:text-white text-4xl">Cloud Services</p>
           <p className="dark:text-white mt-4 text-2xl">
             • Firebase
             <br />• AWS
             <br />• Google Cloud
           </p>
-        </div>
+        </motion.div>
       </div>
     </>
   );
