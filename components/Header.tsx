3a075422a30a00d6d62e4036c67691a8975b1eeb
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-01-11 21:04:56 -0500

    made header cleaner on mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,35 +8,37 @@ import ThemeToggle from './ThemeToggle';
 
 const Header: React.FC = () => {
   return (
-    <header className="w-full fixed top-0 bg-gray-200 dark:bg-gray-800 p-4">
-      <nav className="container mx-auto flex justify-between items-center">
-        <ThemeToggle />
-        <ul className="flex space-x-4">
-          <li>
-            <a
-              href="https://github.com/neilmccaffrey"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faGithub}
-                className="text-4xl dark:text-white transition-transform duration-200 hover:scale-125"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/neil-mccaffrey-2309b116b/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                className="text-4xl text-linkedIn dark:text-white transition-transform duration-200 hover:scale-125"
-              />
-            </a>
-          </li>
-        </ul>
+    <header className="w-full fixed top-0 bg-gray-200 dark:bg-gray-800 p-2 md:p-4">
+      <nav className="container mx-auto flex flex-col items-center md:flex-row md:justify-between">
+        <div className="flex justify-between items-center w-full sm:w-auto md:space-x-96">
+          <ThemeToggle />
+          <ul className="flex space-x-4">
+            <li>
+              <a
+                href="https://github.com/neilmccaffrey"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  className="text-4xl dark:text-white transition-transform duration-200 hover:scale-125"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/neil-mccaffrey-2309b116b/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  className="text-4xl text-linkedIn dark:text-white transition-transform duration-200 hover:scale-125"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
         <ul className="flex space-x-4">
           <li>
             <Link
