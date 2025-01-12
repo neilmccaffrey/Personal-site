141c256b0641c6ccae5409741ffded3695f6860e
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-01-12 15:12:51 -0500

    edited self description

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   const text = 'Hi, I’m Neil McCaffrey';
 
   return (
-    <section className="container mx-auto p-4 dark:text-white min-h-screen flex flex-col justify-center pt-16">
+    <section className="container mx-auto p-4 dark:text-white min-h-screen flex flex-col justify-center pt-20 md:pt-0">
       <h1 className="text-3xl md:text-6xl">
         {text.split('').map((char, index) => (
           <span
@@ -17,17 +17,18 @@ export default function Page() {
           </span>
         ))}
       </h1>
-      <p className="mt-4 text-2xl  dark:text-white">
-        I am a software developer who enjoys creating fullstack mobile and web
-        applications. I like learning new languages and tools, and I have
-        experience using React Native, React.js, Java, Firebase, JavaScript, and
-        more. My focus is on leveraging modern technologies to build efficient
-        and user-friendly applications.
+      <p className="mt-4 text-xl md:text-2xl dark:text-white">
+        I am a front-end software developer with a passion for creating mobile
+        and web applications. While my expertise lies in technologies like React
+        Native, React, and RESTful APIs, I also have experience with backend
+        development, enabling me to build and maintain full-stack applications.
+        My focus is on leveraging modern technologies to build efficient and
+        user-friendly applications.
       </p>
       <section className="container mx-auto p-4 flex flex-col items-center">
         <p className="mt-8 text-xl  dark:text-white">
-          I have developed two applications using React Native that are
-          available for download
+          I have developed and successfully deployed two applications using
+          React Native, both available for download
         </p>
         <Link href="/apps">
           <button
