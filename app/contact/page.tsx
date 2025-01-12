b7ff689ae8c7566608d56d38dcfd77e8139762a8
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-01-11 21:41:08 -0500

    adjustments for mobile

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,8 +5,10 @@ const Contact: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen p-4">
       <main>
-        <h1 className="text-3xl font-bold mb-4 dark:text-white">Contact Me</h1>
-        <p className="mb-6 text-lg dark:text-white">
+        <h1 className="text-xl md:text-3xl font-bold mb-2 md:mb-4 dark:text-white pt-16 md:pt-0">
+          Contact Me
+        </h1>
+        <p className="mb-2 md:mb-6 text-base md:text-lg dark:text-white">
           I'd love to hear from you! Whether you have a question, a project
           idea, or just want to say hi, feel free to get in touch.
         </p>
--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -114,7 +114,7 @@ const ContactForm = () => {
             onChange={handleChange}
             rows={4}
             required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            className="mt-1 block w-full px-3 md:py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
         </div>
 
